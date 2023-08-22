a70dd7ba11c203cf1d1dea46d8735a78ff38071b
maxpipoka <martincardozo@gmail.com>
2023-08-22 12:23:28 -0300

    Catalogo y Producto terminados

--- src/paginas/Catalogo/Catalogo.js
+++ src/paginas/Catalogo/Catalogo.js
@@ -16,7 +16,23 @@ const Catalogo = () => {
             }
         </div>
     );
-
 }
 
 export default Catalogo;
+
+// Este componente tiene por función mostrar una galeria
+// con los productos disponibles en la aplicación.
+// En la línea 1 hace la importación del componente Producto
+// para tenerlo disponible para su utilización.
+// En la linea 2 importa un array con objetos, donde cada objeto
+// es un producto con sus propiedades, dentro de una variable 
+// que se define como listaDeProductos.
+
+// En el método 'return' se renderiza un div que va a ser una grilla
+// que dentro mostrará los diferentes productos.
+// En la linea 9 se abre un extracto de codigo javascript por eso se usa la llave '{'
+// En la linea 10 se le aplica el método 'map' a la variable con los productos,
+// pasándole el producto de la variable sobre el cual se esta iterando
+// y la función devuele el llamado al componente Producto llevando la prop
+// que el componente Producto necesita.
+// LEER EL COMENTARIO EN EL COMPONENTE PRODUCTO PARA MAS REFERENCIA
--- src/paginas/Producto/Producto.js
+++ src/paginas/Producto/Producto.js
@@ -1,5 +1,17 @@
 
-const Producto = (producto) => {
+// Se define un componente Producto el cual se encargará de mostrar
+// cada producto. La información a renderizar de cada producto la recibe
+// mediante prop ({producto}) desde el componente que lo invoca, que es catalogo.
+// Es decir que catalogo llama a producto pasandole props, y producto pinta la
+// data del producto para ser mostrada.
+// En la linea 14 se puede ver como se recibe la prop luego de la definicion de la
+// funcion, y luego se imprime cada uno de los valores que estan dentro de la prop
+// como se puede ver en la linea 19 y 20 por ejemplo.
+
+// Esta estratégia permite reutilizar código, ya que la definición del renderizado
+// de un producto se realiza una sola vez, luego se cambia dinámicamente la informacion.
+
+const Producto = ({producto}) => {
 
     return (
         <div className="card">
@@ -10,11 +22,6 @@ const Producto = (producto) => {
 			<h2>{producto.nombre}</h2>
 			<p>{producto.descripcion}</p>
 			<h3>{producto.precio}</h3>
-			<div className="agregar-carrito">
-					{/* <button>
-						<i className="bx bxs-cart-add "></i>
-					</button> */}
-			</div>
 		</div>
     );
 
